de0e2c5da6ddeefc49882bb92ea8c69c341f61d6
Devang Vangane <devangvangane9@gmail.com>
2025-06-09 20:45:05 +0530

    done for day1

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/About';
+import Create from './components/Create';
+
 
 function App() {
 
 
   return (
     <>
-      <h1>
-        hello people
-      </h1>
+      <Routes>
+        <Route path='' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/create' element={<Create/>}></Route>
+      </Routes>
     </>
   )
 }
