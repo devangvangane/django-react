393dc04b744be838b3afe0e8c8c28b43313ab28d
Devang Vangane <devangvangane9@gmail.com>
2025-06-11 22:39:08 +0530

    Navbar

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,21 +1,28 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/home';
+import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
+import Navbar from './components/Navbar';
 
 
 function App() {
 
 
   return (
-    <>
-      <Routes>
+    <div className='App'>
+      <Navbar drawerWidth={220}
+        content = {
+          <Routes>
         <Route path='' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/create' element={<Create/>}></Route>
       </Routes>
-    </>
+        }
+      
+      />
+      
+    </div>
   )
 }
 
